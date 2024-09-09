06171278822f2a2fbff1e0ac488deb5d6446ec05
Autumn Eckley <eckley.autumn@gmail.com>
2024-09-09 01:37:42 -0600

    Background Working

--- app/controllers/BgPictureController.js
+++ app/controllers/BgPictureController.js
@@ -1,10 +1,13 @@
 
+import { AppState } from "../AppState.js"
 import { bgPictureService } from "../services/BgPictureService.js"
 import { Pop } from "../utils/Pop.js"
+import { setHTML } from "../utils/Writer.js"
 
 export class BgPictureController {
   constructor() {
     console.log('Bg Controller working')
+    AppState.on('picture', this.drawBgPicture)
     this.getBgPicture()
   }
 
@@ -15,5 +18,10 @@ export class BgPictureController {
       Pop.error(error)
       console.error(error)
     }
+  }
+
+  drawBgPicture() {
+    const picture = AppState.picture
+    document.body.style.backgroundImage = `url(${picture.largeImgUrl})`
   }
 }
